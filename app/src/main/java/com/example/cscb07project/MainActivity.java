287433054df723b36cac5b46b3ca84595d730fa1
Zubair <zubair.alnoor28@gmail.com>
2021-11-18 19:09:01 -0500

    This is the first commit. I added a comment.

--- app/src/main/java/com/example/cscb07project/MainActivity.java
+++ app/src/main/java/com/example/cscb07project/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //This is my very first comment
     }
 }
