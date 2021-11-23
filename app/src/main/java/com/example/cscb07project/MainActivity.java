19d887859ce6db912dbe53fb9f75574d42ac535a
Zubair <zubair.alnoor28@gmail.com>
2021-11-23 16:37:27 -0500

    Set up Firebase.

--- app/src/main/java/com/example/cscb07project/MainActivity.java
+++ app/src/main/java/com/example/cscb07project/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -18,5 +21,11 @@ public class MainActivity extends AppCompatActivity {
 
         //Merge check
         //aaaaaaa
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference();
+
+        myRef.child("Users").child("FirstUser").child("Password").setValue("FirstPass");
     }
 }
