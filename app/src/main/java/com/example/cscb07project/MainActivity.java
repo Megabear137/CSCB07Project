07f0a53e0992c4fb61709fbe6277f551b5387b5f
boni-teppanyaki <hepintao81620@163.com>
2021-11-24 16:04:41 +0800

    setup of first screen

--- app/src/main/java/com/example/cscb07project/MainActivity.java
+++ app/src/main/java/com/example/cscb07project/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.cscb07project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -26,7 +28,18 @@ public class MainActivity extends AppCompatActivity {
         // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference();
-
-
     }
+
+    public void navigateToLogin(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void navigateToSignup(View view) {
+        Intent intent = new Intent(this, SignupActivity.class);
+        startActivity(intent);
+    }
+
+
+
 }
